959cb0904d16380bec5938e8fc2eb02c44d546b4
Jakub Strnad <jakub.strnad@protonmail.com>
2022-07-23 03:25:35 +0200

    More reasonable defaults

--- zsh-aliases-exa.plugin.zsh
+++ zsh-aliases-exa.plugin.zsh
@@ -9,13 +9,12 @@
 #
 
 # general use
-alias ls='exa'                                                         # ls
-alias l='exa -lbF --git'                                               # list, size, type, git
-alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+alias ls='exa' # ls
+alias la='exa -bhgmaGF --time-style=iso --git --color-scale' # ls -a
+alias l='exa -lbhgmGF --git --icons' # ls -l
+alias ll='exa -lbhgmaGF --time-style=iso --git --color-scale --icons' # ls -la
+alias llm='exa -lbhgmaGF --git --sort=modified' # long list, modified date sort
 
 # speciality views
-alias lS='exa -1'			                                                  # one column, just names
-alias lt='exa --tree --level=2'                                         # tree
+alias lS='exa -1' # one column, just names
+alias lt='exa --tree --level=2' # tree
